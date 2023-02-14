95453df65cf18d1fb694e437459be14a24748aac
Ben Kerns <benkerns@pm.me>
2023-02-13 19:49:52 -0700

    Added MathHelper class to Tools project. Added Tools.Tests Project. Added MathHelperTests class to Tools.Tests Project.

---
+++ Tools.Tests/MathHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace Tools.Tests
+{
+  [TestClass]
+  public class MathHelperTests
+  {
+    [TestMethod]
+    public void LawOfCosinesForGammaTest()
+    {
+      Assert.IsTrue(MathHelper.LawOfCosinesForGamma(3, 4, 5) == 1.5707963267948966);
+      Assert.IsTrue(MathHelper.LawOfCosinesForGamma(20, 22, 18) == 0.881021326009397);
+    }
+  }
+}
---
+++ Tools/MathHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace Tools
+{
+  public class MathHelper
+  {
+    #region Trigonometry
+
+    public static double ConvertRadiansToDegrees(double radians)
+    {
+      return (180 / Math.PI) * radians;
+    }
+
+    /// <summary>
+    /// Uses the law of cosines to find the angle between 'a' and 'b' for a triangle.
+    /// </summary>
+    /// <returns>The measure of the angle between 'a' and 'b' in radians.</returns>
+    public static double LawOfCosinesForGamma(double a, double b, double c)
+    {
+      return Math.Acos((Math.Pow(a, 2) + Math.Pow(b, 2) - Math.Pow(c, 2))/(2 * a * b));
+    }
+    #endregion
+  }
+}
